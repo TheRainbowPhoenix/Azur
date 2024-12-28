6d970be70a77d467b10bc82169dd63379fc1df51
Lephenixnoir <sebastien.michelland@protonmail.com>
2024-12-28 22:27:48 +0100

    gl: add grayscale option to 2D texture shader

--- azur/glsl/fs_tex2d.glsl
+++ azur/glsl/fs_tex2d.glsl
@@ -1,4 +1,5 @@
 in vec2 v_position;
+flat in int v_grayscale;
 
 _GL(out vec4 color;)
 
@@ -6,8 +7,10 @@ uniform sampler2D u_image;
 
 void main() {
 	#ifdef GL_ES
-	gl_FragColor = texture2D(u_image, v_position);
+	vec4 t = texture2D(u_image, v_position);
+	gl_FragColor = v_grayscale != 0 ? vec4(t.r, t.r, t.r, t.a) : t;
 	#else
-	color = texture(u_image, v_position);
+	vec4 t = texture(u_image, v_position);
+	color = v_grayscale != 0 ? vec4(t.r, t.r, t.r, t.a) : t;
 	#endif
 }
--- azur/glsl/vs_tex2d.glsl
+++ azur/glsl/vs_tex2d.glsl
@@ -2,13 +2,17 @@
 layout(location=0) in vec2 a_vertex;
 /* Same in texture space */
 layout(location=1) in vec2 a_texture_pos;
+/* Whether to replicate red on all channels */
+layout(location=2) in int a_grayscale;
 
 /* Location in image space */
 out vec2 v_position;
+flat out int v_grayscale;
 
 uniform mat3 u_transform;
 
 void main() {
     v_position = a_texture_pos;
+    v_grayscale = a_grayscale;
     gl_Position = vec4((u_transform * vec3(a_vertex, 1.0)).xy, 0.0, 1.0);
 }
