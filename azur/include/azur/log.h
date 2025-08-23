63650bb49cf98631d82737ed5809c67f2be43969
Lephenixnoir <sebastien.michelland@protonmail.com>
2025-08-23 23:45:29 +0200

    azur: start revamping logging interface to support web console output
    Still to do:
    * Properly formalize log filters
    * Put the globals in the future application class

--- azur/include/azur/log.h
+++ azur/include/azur/log.h
@@ -18,6 +18,16 @@
 #pragma once
 #include <azur/defs.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <string>
+
+/* Quick macros that insert parameters automatically. */
+#define azlog(LEVEL, FMT, ...) \
+    azur::log::write(azur::log::LogLevel::LEVEL, \
+        {__FILE__, __LINE__, __func__}, FMT, ## __VA_ARGS__)
+
+#define azlog_begin() azur::log::output()->beginGroup()
+#define azlog_end() azur::log::output()->endGroup()
 
 namespace azur::log {
 
@@ -33,15 +43,17 @@ enum class LogLevel { DEBUG, INFO, WARN, ERROR, FATAL };
    ANSI:   ANSI escape sequences; suitable for terminals. */
 enum class LogStyle { PLAIN, ANSI };
 
+// TODO: Put logger in current app
+class LogOutput;
+struct LogLocation;
+/* Current log output handler; never NULL (defaults to stderr). */
+LogOutput *output();
+
 /* Produce a log message. See also azlog() and azlogc() for shorter versions.
    level:  Message level
-   file:   File name (normally __FILE__)
-   line:   Line number (normally __LINE__)
-   func:   Function name (normally __func__)
-   cont:   Whether this is the continuation of a previous message
+   loc:   Location of the log call in source code
    fmt...: print()-style format */
-void write(LogLevel level, char const *file, int line, char const *func,
-    bool cont, char const *fmt, ...);
+void write(LogLevel level, LogLocation const &loc, char const *fmt, ...);
 
 /* Get the minimum-level filter. Messages below the minimum level are filtered
    out. The default is INFO in release builds, DEBUG in debug builds. */
@@ -50,21 +62,101 @@ LogLevel minimumLevelFilter();
 /* Set the minimum-level filter. */
 void setMinimumLevelFilter(LogLevel level);
 
-/* Get the output stream for log messages. Default is stderr. */
-FILE *outputStream();
+/* Location in code from which a log message is emitted. */
+struct LogLocation
+{
+    char const *file;       /* File name, usually __FILE__ */
+    int line;               /* Line number, usually __LINE__ */
+    char const *function;   /* Function name, usually __func__ */
+};
 
-/* Set the output stream for log messages. Since the library might output
-   messages until very late in execution, a valid output stream should always
-   be specified. When using a file, stderr should be set as the output stream
-   before closing the file at the end of execution. */
-void setOutputStream(FILE *fp, LogStyle style);
+/* An output for logged messages (terminal, file, web console, etc). */
+class LogOutput
+{
+public:
+    /* Produce a log message with a format and a `va_list`.
+       level: Message level
+       loc:   Location of the log call in source code
+       fmt:   print()-style format
+       args:  List of arguments for fmt*/
+    virtual void writev(LogLevel level, LogLocation const &loc,
+                        char const *fmt, va_list args) = 0;
+
+    /* Produce a log message with a format and variadic arguments. */
+    void write(LogLevel level, LogLocation const &loc, char const *fmt, ...) {
+        va_list args;
+        va_start(args, fmt);
+        writev(level, loc, fmt, args);
+        va_end(args);
+    }
+
+    /* Begin or end a message group. Messages between calls to `beginGroup()`
+       and `endGroup()` should be treated as one. Typically, the level and
+       location prefix is only shown for the first message. Outputs might give
+       additional semantics to groups. Groups are a convenience for splitting
+       unitary log messages into multiple calls and cannot be nested. */
+    virtual void beginGroup() = 0;
+    virtual void endGroup() = 0;
+};
+
+/* Terminal and file output for logged messages. */
+class LogOutputFile: public LogOutput
+{
+public:
+    LogOutputFile(FILE *fp, bool closeFilePointer, bool enableAnsiEscapes);
+    ~LogOutputFile();
+
+    void writev(LogLevel level, LogLocation const &loc, char const *fmt,
+                va_list args) override;
+    void beginGroup() override;
+    void endGroup() override;
+
+private:
+    /* File pointer to output stream */
+    FILE *m_fp;
+    /* Whether the stream should be closed when destroying the logger */
+    bool m_closeFilePointer;
+    /* Whether to enable ANSI escapes-based colors */
+    bool m_ansiEscapes;
+    /* Size of the current group; -1 when not in a group. */
+    int m_groupSize = -1;
+
+    /* Whether we're currently in a group. */
+    bool inGroup() { return m_groupSize >= 0; }
+};
+
+#if AZUR_PLATFORM_EMSCRIPTEN
+
+/* Web output in emscripten using the Javascript console API. */
+class LogOutputJSConsole: public LogOutput
+{
+public:
+    void writev(LogLevel level, LogLocation const &loc, char const *fmt,
+                va_list args) override;
+    void beginGroup() override;
+    void endGroup() override;
+
+private:
+    /* String accumulated so for for messages in a group. */
+    std::string m_groupString;
+    /* Size of the current group; -1 when not in a group. */
+    int m_groupSize = -1;
+    /* Log level of the current group; undefined when not in a group. */
+    LogLevel m_groupLevel;
+    /* Prefix string of the current group; empty when not in a group. */
+    std::string m_groupPrefix;
+
+    /* Whether we're currently in a group. */
+    bool inGroup() { return m_groupSize >= 0; }
+    /* Prefix string for a level and location. */
+    std::string prefixString(LogLevel level, LogLocation const &loc);
+    /* Output to console. */
+    void output(LogLevel level, std::string const &prefix, char const *data);
+};
+
+#endif
+
+// TODO: LogOutputBuffer class with a rotating buffer, to display logs in GUI
+// TODO: Application-specific type integer in logs API
 
 } /* namespace azur::log */
-
-/* Quick macros that insert parameters automatically. */
-#define azlog(LEVEL, FMT, ...) \
-    azur::log::write(azur::log::LogLevel::LEVEL, __FILE__, __LINE__, \
-        __func__, false, FMT, ## __VA_ARGS__)
-#define azlogc(LEVEL, FMT, ...) \
-    azur::log::write(azur::log::LogLevel::LEVEL, __FILE__, __LINE__, \
-        __func__, true, FMT, ## __VA_ARGS__)
--- azur/src/log.cpp
+++ azur/src/log.cpp
@@ -8,6 +8,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <azur/ecs.h>
+
+#if AZUR_PLATFORM_EMSCRIPTEN
+# include <emscripten.h>
+#endif
 
 namespace azur::log {
 
@@ -17,48 +22,32 @@ namespace azur::log {
  static LogLevel levelFilter = LogLevel::INFO;
 #endif
 
-#if AZUR_TERMINAL_ANSI
- static LogStyle style = LogStyle::ANSI;
-#else
- static LogStyle style = LogStyle::PLAIN;
-#endif
-
-static FILE *fp = stderr;
-
 #define ANSI_RED        "\e[31m"
 #define ANSI_CYAN       "\e[36m"
 #define ANSI_YELLOW     "\e[33m"
 #define ANSI_CLEAR      "\e[0m"
 
-void write(LogLevel level, char const *file, int line, char const *func,
-    bool cont, char const *fmt, ...)
+#if AZUR_PLATFORM_EMSCRIPTEN
+static LogOutputJSConsole LOJSC;
+static LogOutput *LO = &LOJSC;
+#else
+static LogOutputFile LOF(stderr, false, (bool)AZUR_TERMINAL_ANSI);
+static LogOutput *LO = &LOF;
+#endif
+
+LogOutput *output()
+{
+    return LO;
+}
+
+void write(LogLevel level, LogLocation const &loc, char const *fmt, ...)
 {
     if(level < levelFilter)
         return;
 
-    char const *RED     = ANSI_RED;
-    char const *CYAN    = ANSI_CYAN;
-    char const *YELLOW  = ANSI_YELLOW;
-    char const *CLEAR   = ANSI_CLEAR;
-
-    if(style == LogStyle::PLAIN)
-        RED = CYAN = YELLOW = CLEAR = "";
-
-    if(!cont) {
-        if(level == LogLevel::DEBUG)
-            fprintf(fp, "%s%s:%d:", CYAN, file, line);
-
-        fprintf(fp, "%s%s:%s ", YELLOW, func, CLEAR);
-
-        if(level == LogLevel::ERROR)
-            fprintf(fp, "%serror:%s ", RED, CLEAR);
-        if(level == LogLevel::FATAL)
-            fprintf(fp, "%sfatal error:%s ", RED, CLEAR);
-    }
-
     va_list args;
     va_start(args, fmt);
-    vfprintf(fp, fmt, args);
+    output()->writev(level, loc, fmt, args);
     va_end(args);
 
     if(level == LogLevel::FATAL)
@@ -75,15 +64,164 @@ void setMinimumLevelFilter(LogLevel lv)
     levelFilter = lv;
 }
 
-FILE *outputStream()
+//=== File/terminal output ===================================================//
+
+LogOutputFile::LogOutputFile(
+    FILE *fp, bool closeFilePointer, bool enableAnsiEscapes):
+    m_fp {fp}, m_closeFilePointer {closeFilePointer},
+    m_ansiEscapes {enableAnsiEscapes}
+{}
+
+LogOutputFile::~LogOutputFile()
 {
-    return fp;
+    if(m_closeFilePointer)
+        fclose(m_fp);
+    m_fp = nullptr;
 }
 
-void setOutputStream(FILE *new_fp, LogStyle new_style)
+void LogOutputFile::writev(
+    LogLevel level, LogLocation const &loc, char const *fmt, va_list args)
 {
-    fp = new_fp;
-    style = new_style;
+    char const *RED     = m_ansiEscapes ? ANSI_RED : "";
+    char const *CYAN    = m_ansiEscapes ? ANSI_CYAN : "";
+    char const *YELLOW  = m_ansiEscapes ? ANSI_YELLOW : "";
+    char const *CLEAR   = m_ansiEscapes ? ANSI_CLEAR : "";
+
+    /* Skip the prefix if this is a group and not the first message */
+    if(!inGroup() || m_groupSize == 0) {
+        if(level == LogLevel::DEBUG)
+            fprintf(m_fp, "%s%s:%d:", CYAN, loc.file, loc.line);
+
+        fprintf(m_fp, "%s%s:%s ", YELLOW, loc.function, CLEAR);
+
+        if(level == LogLevel::ERROR)
+            fprintf(m_fp, "%serror:%s ", RED, CLEAR);
+        if(level == LogLevel::FATAL)
+            fprintf(m_fp, "%sfatal error:%s ", RED, CLEAR);
+    }
+
+    vfprintf(m_fp, fmt, args);
+
+    // TODO: Exiting on fatal error isn't the responsibility of LogOutput
+    if(level == LogLevel::FATAL)
+        exit(1);
+
+    m_groupSize += inGroup();
 }
+
+void LogOutputFile::beginGroup()
+{
+    m_groupSize = 0;
+}
+
+void LogOutputFile::endGroup()
+{
+    m_groupSize = -1;
+}
+
+//=== emscripten web console output ==========================================//
+
+#if AZUR_PLATFORM_EMSCRIPTEN
+
+void LogOutputJSConsole::writev(
+    LogLevel level, LogLocation const &loc, char const *fmt, va_list args)
+{
+    char *data = NULL;
+    char const *fallback = "<vasprintf error>\n";
+    vasprintf(&data, fmt, args);
+
+    /* For messages outside of a group, print directly */
+    if(!inGroup()) {
+        output(level, prefixString(level, loc), data ? data : fallback);
+    }
+    else {
+        /* For the first message of a group, record group info */
+        if(m_groupSize == 0) {
+            m_groupPrefix = prefixString(level, loc);
+            m_groupLevel = level;
+        }
+
+        /* Accumulate message data */
+        m_groupString += (data ? data : fallback);
+        m_groupSize++;
+    }
+
+    free(data);
+
+    // TODO: Exiting on fatal error isn't the responsibility of LogOutput
+    if(level == LogLevel::FATAL)
+        exit(1);
+}
+
+void LogOutputJSConsole::beginGroup()
+{
+    m_groupSize = 0;
+    m_groupString = "";
+}
+
+void LogOutputJSConsole::endGroup()
+{
+    if(m_groupSize > 0)
+        output(m_groupLevel, m_groupPrefix, m_groupString.c_str());
+
+    m_groupSize = -1;
+    m_groupString = "";
+}
+
+std::string LogOutputJSConsole::prefixString(
+    LogLevel level, LogLocation const &loc)
+{
+    char str[32];
+
+    std::string prefix = "%c"; /* cyan */
+    if(level == LogLevel::DEBUG) {
+        prefix += loc.file;
+        snprintf(str, sizeof str, ":%d: ", loc.line);
+        prefix += str;
+    }
+
+    prefix += "%c"; /* gray */
+    prefix += loc.function ? loc.function : "<anonymous>";
+    prefix += ":";
+
+    prefix += "%c"; /* normal */
+    if(level == LogLevel::FATAL)
+        prefix += " FATAL ERROR: ";
+    return prefix;
+}
+
+/* All prefixes follow the same generic format for colors with a fixed number
+   of color changes (%c); this avoids variations in the number of parameters.
+     <debug>: <function>: <message>
+     ^cyan    ^gray       ^normal */
+#define CONSOLE_CALL(NAME, PREFIX, MESSAGE) \
+    EM_ASM({ \
+        console.NAME(UTF8ToString($0), 'color:#21797d', 'color:#606060', ' ', \
+                     UTF8ToString($1)); \
+    }, PREFIX, MESSAGE)
+
+void LogOutputJSConsole::output(
+    LogLevel level, std::string const &prefix, char const *message)
+{
+    switch(level) {
+    case LogLevel::ERROR:
+    case LogLevel::FATAL:
+        CONSOLE_CALL(error, prefix.c_str(), message);
+        break;
+    case LogLevel::WARN:
+        CONSOLE_CALL(warn, prefix.c_str(), message);
+        break;
+    case LogLevel::INFO:
+        CONSOLE_CALL(info, prefix.c_str(), message);
+        break;
+    case LogLevel::DEBUG:
+        CONSOLE_CALL(debug, prefix.c_str(), message);
+        break;
+    default:
+        CONSOLE_CALL(log, prefix.c_str(), message);
+    }
+}
+
+#endif
 
 } /* namespace azur::log */
