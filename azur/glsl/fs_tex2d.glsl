70f4fdc10518eab6bca5783215c266a2123547d0
Lephenixnoir <sebastien.michelland@protonmail.com>
2025-08-25 10:20:30 +0200

    azur: fix tex2d shader on OpenGL ES 3.0
    Feels so good to not have to check manual for every small difference.

--- azur/glsl/fs_tex2d.glsl
+++ azur/glsl/fs_tex2d.glsl
@@ -1,16 +1,11 @@
 in vec2 v_position;
 in float v_grayscale;
 
-_GL(out vec4 color;)
+out vec4 color;
 
 uniform sampler2D u_image;
 
 void main() {
-	#ifdef GL_ES
-	vec4 t = texture2D(u_image, v_position);
-	gl_FragColor = v_grayscale > 0.5 ? vec4(t.r, t.r, t.r, t.a) : t;
-	#else
 	vec4 t = texture(u_image, v_position);
 	color = v_grayscale > 0.5 ? vec4(t.r, t.r, t.r, t.a) : t;
-	#endif
 }
