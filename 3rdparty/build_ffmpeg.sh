573b5e647f96fd8bd16affdb7a7fab826eb08722
Lephenixnoir <sebastien.michelland@protonmail.com>
2025-12-13 12:01:56 +0100

    ffmpeg: fully clean up ffmpeg build
    It's quite minimal but it still adds 3-4 MB to the MQ binary.

--- 3rdparty/build_ffmpeg.sh
+++ 3rdparty/build_ffmpeg.sh
@@ -13,63 +13,60 @@ set -e
 rm -rf "$PREFIX"
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 
-echo "=== Configuring x264 ==="
+header() {
+  echo -e "\e[33;1m<ffmpeg> $1\e[37;3m ===================================\e[0m"
+}
+
+header "Configuring x264"
 
 rm -rf "$X264_BUILD"
 mkdir -p "$X264_BUILD"
 cd "$X264_BUILD"
 "$X264_SOURCE/configure" --prefix="$PREFIX" \
   --disable-cli \
-  --enable-static
+  --enable-static \
+  --extra-cflags="-Os -ffunction-sections"
 
-echo "=== Building x264 ==="
+header "Building x264"
 
 make -j$(nproc) all install
 
-echo "=== Configuring ffmpeg ==="
+header "Configuring ffmpeg"
 
 rm -rf "$FFMPEG_BUILD"
 mkdir -p "$FFMPEG_BUILD"
 cd "$FFMPEG_BUILD"
 
 "$FFMPEG_SOURCE/configure" --prefix="$PREFIX" \
+  --disable-everything \
   --disable-programs \
-  --disable-encoders \
-  --disable-decoders \
-  --disable-hwaccels \
-  --disable-bsfs \
-  --disable-protocols \
-  --disable-indevs \
-  --disable-outdevs \
-  --disable-filters \
-  --disable-muxers \
-  --disable-demuxers \
   --disable-doc \
   --disable-autodetect \
+  --enable-small \
+  --extra-cflags="-ffunction-sections" \
   --enable-gpl \
-  --enable-gpl \
-  --enable-libx264 \
-  --enable-libx265 \
-  --enable-libvpx \
-  --enable-encoder=libx264 \
-  --enable-decoder=h264 \
   --enable-avcodec \
   --enable-avformat \
   --enable-swscale \
   --enable-protocol=file \
-  --enable-bsf=mjpeg2jpeg \
-  --enable-muxer=avi --enable-muxer=h264 --enable-muxer=mjpeg --enable-muxer=mp4 \
-  --enable-demuxer=h264 --enable-demuxer=m4v --enable-demuxer=mp3 --enable-demuxer=mpegvideo --enable-demuxer=mpegps --enable-demuxer=mjpeg --enable-demuxer=mov --enable-demuxer=avi --enable-demuxer=aac --enable-demuxer=pmp --enable-demuxer=oma --enable-demuxer=pcm_s16le --enable-demuxer=pcm_s8 --enable-demuxer=wav
+  --enable-libx264 \
+  --enable-encoder=libx264 \
+  --enable-decoder=h264 \
+  --enable-muxer=avi --enable-muxer=h264 --enable-muxer=mjpeg \
+    --enable-muxer=mp4 \
+  --enable-demuxer=h264 --enable-demuxer=m4v --enable-demuxer=mp3 \
+    --enable-demuxer=mpegvideo --enable-demuxer=mjpeg --enable-demuxer=avi \
+    --enable-demuxer=aac --enable-demuxer=wav \
+  --enable-parser=h264 --enable-parser=mpeg4video --enable-parser=mpegaudio \
+    --enable-parser=mpegvideo --enable-parser=aac --enable-parser=aac_latm
 
-# --disable-everything
-# --enable-parser=h264 --enable-parser=mpeg4video --enable-parser=mpegaudio --enable-parser=mpegvideo --enable-parser=mjpe--enable-parser=aac --enable-parser=aac_latm
-
-
+#  --enable-libx265 \
+#  --enable-libvpx \
 #  --enable-encoder=libx265 \
 #  --enable-encoder=libvpx_vp9 \
 #  --enable-decoder=hevc \
 #  --enable-decoder=libvpx_vp9 \
 
-echo "=== Building ffmpeg ==="
+header "Building ffmpeg"
 
 make -j$(nproc) all install
